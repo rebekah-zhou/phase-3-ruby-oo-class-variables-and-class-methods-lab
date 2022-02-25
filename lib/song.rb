8815b850a639d23dee1febd79826773200eea29c
Rebekah Zhou <rebekahzhou@gmail.com>
2022-02-25 14:29:27 -0800

    commit tree d50e156856bb76c0e4f80c3cad17f4ffb9081951

---
+++ lib/song.rb
@@ -0,0 +1,47 @@
+class Song
+    attr_accessor :name, :artist, :genre
+
+    @@count = 0
+    @@artists = []
+    @@genres = []
+
+    def initialize(name, artist, genre)
+        @name = name
+        @artist = artist 
+        @genre = genre
+        @@count += 1
+        @@artists << artist
+        @@genres << genre
+    end
+
+    def self.artists
+        @@artists
+    end
+
+    def self.genres
+        @@genres
+    end
+
+    def self.artist_count
+        @@artists.tally
+    end
+
+    def self.count
+        @@count
+    end
+
+    def self.genres
+        @@genres.uniq
+    end
+
+    def self.artists
+        @@artists.uniq
+    end
+
+    def self.genre_count 
+        @@genres.tally
+    end
+
+
+
+end
